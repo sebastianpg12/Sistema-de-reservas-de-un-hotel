440284b387006054f566b4e1ea2bd6446daf1686
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-03-23 08:18:48 -0500

    Creación de plantilla de proyecto con Spring boot Initializr

---
+++ SistemaReservasHotel/src/main/java/co/com/sofka/SistemaReservasHotel/SistemaReservasHotelApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.SistemaReservasHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservasHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservasHotelApplication.class, args);
+	}
+
+}
---
+++ SistemaReservasHotel/src/test/java/co/com/sofka/SistemaReservasHotel/SistemaReservasHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.SistemaReservasHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservasHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
